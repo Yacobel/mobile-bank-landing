70127d7addb8783fb4462a98448e163e8d839299
Yacobel <yacobelhaouarii@gmail.com>
2025-05-09 11:23:54 -0700

    Format click event listener for consistency and readability

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 let icone = document.getElementById("icone");
 let ul = document.getElementById("ul");
-icone.addEventListener("click",()=> {
-    console.log('hhhh');
-    
-  ul.classList.toggle('show');
-  icone.style.zIndex='1'
+icone.addEventListener("click", () => {
+  console.log("hhhh");
+  ul.classList.toggle("show");
+  icone.style.zIndex = "1";
 });
